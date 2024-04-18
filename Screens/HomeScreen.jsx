e7af836a689d07588dd5f4b169783f709f37aadb
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-17 22:14:09 -0600

    Updated

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -206,7 +206,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     width: '80%',
     alignItems: 'center',
-    backgroundColor: 'white', 
+    backgroundColor: '#fffd', 
     },
   
 });
